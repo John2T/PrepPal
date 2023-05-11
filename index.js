7dbdeaed36d635549959983ff5d47d90481ab873
Lucas <113465142+LucasHYing@users.noreply.github.com>
2023-05-10 23:32:38 -0700

    added settings, navigation bar and personal profiles page, but has bugs.

---
+++ index.js
@@ -0,0 +1,221 @@
+
+require("./utils.js");
+
+require('dotenv').config();
+const url=require('url');
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+const port = process.env.PORT || 3030;
+
+const app = express();
+
+const Joi = require("joi");
+
+const { ObjectId } = require('mongodb');
+
+
+const expireTime = 24 * 60 * 60 * 1000; //expires after 1 hour  (hours * minutes * seconds * millis)
+
+/* secret information section */
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+/* END secret section */
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('users');
+
+app.set('view engine', 'ejs');
+
+
+app.use(express.urlencoded({extended: false}));
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://Ly:ly@cluster0.owu76cn.mongodb.net/?retryWrites=true&w=majority`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore, //default is memory store 
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
+
+//---------------------------------------home page------------------------------
+app.get('/', (req, res) => {
+  console.log(req.url);
+  console.log(url.parse(req.url).pathname);
+  const loggedin = req.session.loggedin;
+  const username = req.session.username || '';
+  res.render('index', { loggedin, username });
+});
+
+
+
+// Define a schema for validating user input
+const schema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.post('/signup', async (req, res) => {
+  try {
+    // Validate the user input
+    const { error, value } = schema.validate(req.body);
+    if (error) {
+      throw new Error(error.details[0].message);
+    }
+
+    // Check if the email is already in use
+    const existingUser = await userCollection.findOne({ email: value.email });
+    if (existingUser) {
+      throw new Error('Email address is already in use');
+    }
+
+    // Hash the password using bcrypt
+    const hashedPassword = await bcrypt.hash(value.password, 10);
+
+    // Create a new user in the database
+    const newUser = {
+      name: value.name,
+      email: value.email,
+      password: hashedPassword,
+      user_type: "user",
+    };
+    await userCollection.insertOne(newUser);
+
+    // Create a session for the new user
+    req.session.loggedin = true;
+    req.session.username = value.name;
+
+    // Redirect to the home area
+    res.redirect('/home');
+  } catch (err) {
+    // Display an error message if something went wrong
+    const errorMessage = err.message;
+    res.render('signup', { errorMessage });
+  }
+});
+
+app.get('/home', (req, res) => {
+  if (!req.session.loggedin) {
+    // User is not logged in, redirect to home page
+    res.redirect('/');
+  } else {
+    // User is logged in, display home page
+    var username = req.session.username;
+    res.render('home', { username });
+  }
+});
+
+
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+
+app.get('/nosql-injection', async (req,res) => {
+	var username = req.query.user;
+
+	if (!username) {
+		res.send(`<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`);
+		return;
+	}
+	console.log("user: "+username);
+
+	const schema = Joi.string().max(20).required();
+	const validationResult = schema.validate(username);
+
+	if (validationResult.error != null) {  
+	   console.log(validationResult.error);
+	   res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
+	   return;
+	}	
+
+	const result = await userCollection.find({username: username}).project({username: 1, password: 1, _id: 1}).toArray();
+
+	console.log(result);
+
+    res.send(`<h1>Hello ${username}</h1>`);
+});
+
+
+app.get('/login', (req, res) => {
+  res.render('login', { errorMessage: null});
+});
+
+app.post('/login', async (req, res) => {
+  const { error } = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required()
+  }).validate(req.body);
+
+  if (error) {
+    res.status(400).send('Invalid input');
+    return;
+  }
+
+  const { email, password } = req.body;
+  const user = await userCollection.findOne({ email });
+
+  if (user && await bcrypt.compare(password, user.password)) {
+    req.session.loggedin = true;
+    req.session.username = user.name;
+    req.session.user = user;
+    res.redirect('/home');
+  } else {
+    res.render('login', { errorMessage: 'User and password not found.'});
+  }
+});
+
+
+app.get('/recipe', (req, res) => {
+  res.render('recipe');
+});
+
+app.get('/personal', (req, res) => {
+  res.render('personal');
+});
+
+app.get('/settings', (req, res) => {
+  res.render('settings');
+});
+
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req, res) => {
+  res.status(404);
+  res.render("404", { title: "Page Not Found",navLinks,currentURL: url.parse(req.url).pathname});
+});
+
+
+app.listen(port, () => {
+	console.log("Node application listening on port "+port);
+}); 
